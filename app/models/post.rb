37256872a0594cb0be2a2a80bbc1c955f8ee7aa6
Jordi Carres <jcarres@mdsol.com>
2012-12-22 11:06:44 +0900

    blog posts are done in markdown

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  #TODO: save this in the DB when I can make migrations
+  def html
+    render_contents(self.content).html_safe
+  end
+  
+  private
+  def render_contents(src)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    redcarpet.render src
+  end
 end
