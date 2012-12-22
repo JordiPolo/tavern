91f30d20a5d74e88ab2bf7341b74861b7d8cb41f
Jordi Carres <jcarres@mdsol.com>
2012-12-22 12:35:11 +0900

    added an About page and cleaned up markdown generation

---
+++ lib/markdown.rb
@@ -0,0 +1,22 @@
+class Markdown
+  #this method is used by rails to automatically conver .md files to html
+  def self.call(template)
+    #we call inspect so it is quoted and considered ruby code as Rails expect that
+    self.render(template.source).inspect
+  end
+
+  #human readable method
+  def self.render(src)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true 
+    }
+    redcarpet = Redcarpet::Markdown.new(renderer, options)
+    redcarpet.render( src ).html_safe
+  end
+end
