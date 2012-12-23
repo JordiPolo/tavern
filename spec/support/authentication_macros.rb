c027f5ffb49328f62f8c034d70be85e82eeebd69
Jordi Polo <mumismo@gmail.com>
2012-12-23 10:29:32 +0900

    trying to make test pass... not yet

--- spec/support/authentication_macros.rb
+++ spec/support/authentication_macros.rb
@@ -1,7 +1,13 @@
 module AuthenticationMacros
   def login
-    #Need to  mock authorization
-    @controller = SessionsController.new
-    post :create
+    auth = mock(Authentication)
+    Authentication.stub(:new).and_return(auth)
+    Authentication.stub(:create_new_auth_headers).and_return("")
+    auth.stub(:requested?).and_return(true)
+    auth.stub(:authenticated?).and_return(true)
+    user = create(:user)
+    user.save
+    auth.stub!(:user).and_return(user)
+    post '/session'
   end
 end
